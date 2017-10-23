40759358d964e585de21887defb56f3e50a3e925
surron <31905175+surron@users.noreply.github.com>
2017-10-22 21:32:38 -0700

    Add files via upload

---
+++ appFrame/app.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// using a function contructor form to create an object
+function MyApp()
+{
+	var version = "v1.0";
+
+	// creating a private function
+	function setStatus(message)
+	{
+		$("#app>footer").text(message);
+	}
+
+	// creating a public function
+	this.start = function()
+	{
+		$("#app>header").append(version);
+		setStatus("ready");
+	};
+} // end MyApp
+
+/* 	JQuery's shorthand for the document ready event handler
+		could be written: $(document).ready(handler);
+
+		When this page loads, we'll create a global variable
+		named "app" by attaching it to the "window" object
+		(part of the BOM - Browser Object Model)
+*/
+$(function() {
+	window.app = new MyApp();
+	window.app.start();
+});
